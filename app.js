f351583deb0cc8a3413e59757914ebd978b0b589
Sergey Dedikov <serded59@yandex.ru>
2022-01-12 19:43:34 +0500

    feat: MongoDB

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const { PORT = 3000 } = process.env;
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/mestodb", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
