dcd401af164fc088b308ff9aee973c625eef9430
Sergey Dedikov <serded59@yandex.ru>
2022-01-12 21:14:58 +0500

    feat: controllers and routes for card

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const { PORT = 3000 } = process.env;
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cardsRoutes = require("./routes/cards");
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cardsRoutes);
 
 mongoose.connect("mongodb://localhost:27017/mestodb", {
   useNewUrlParser: true,
---
+++ controllers/cards.js
@@ -0,0 +1,23 @@
+const Card = require("../models/card");
+
+const getCards = (req, res) => {
+  return Card.find({}).then((cards) => {
+    res.status(200).send(cards);
+  });
+};
+
+const createCard = (req, res) => {
+  const { name, link } = req.body;
+
+  return Card.create({ name, link }).then((card) => res.status(201).send(card));
+};
+
+const deleteCard = (req, res) => {
+  Card.findByIdAndRemove(req.params.cardId).then((card) => res.send(card));
+};
+
+module.exports = {
+  getCards,
+  createCard,
+  deleteCard,
+};
